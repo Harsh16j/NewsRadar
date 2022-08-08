fd9c3d8797a735a6f1c9970ed676cd4e624047c2
Harsh <harsh.jethwani16@gmail.com>
2022-08-07 19:23:39 -0500

    Fetching news from News API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageURL, newsURL}=this.props;
-    console.log(typeof description)
+    let { title, description, imageURL, newsURL } = this.props;
     return (
       <div className="my-3">
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={imageURL} style={{ width:'100%',
-    height:'200px'}}/>
-      <Card.Body>
-        <Card.Title>{title.slice(0,45)}</Card.Title>
-        <Card.Text>
-          {description==null?"---":description.slice(0,88)}
-        </Card.Text>
-        <Button variant="primary" size="sm" href={newsURL} target="_blank">Read more</Button>
-      </Card.Body>
-    </Card>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={imageURL}
+            style={{ width: "100%", height: "200px" }}
+          />
+          {/* To fix the size of the image w.r.t. the cards */}
+          <Card.Body>
+            <Card.Title>{title.slice(0, 45)}</Card.Title>
+            <Card.Text>
+              {description == null ? "---" : description.slice(0, 88)}
+            </Card.Text>
+            <Button variant="primary" size="sm" href={newsURL} target="_blank">
+              Read more
+            </Button>
+          </Card.Body>
+        </Card>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
