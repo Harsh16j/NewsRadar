6c12020246abd0a625028f32af28e86f4915115b
Harsh <harsh.jethwani16@gmail.com>
2022-08-08 21:46:19 -0500

    Router created and news categories added in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react'
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { Component } from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 export default class NavBar extends Component {
   render() {
     return (
-      
-        <Navbar bg="light" expand="lg">
-            
-      {/* <Container style={{ width: "100%" }}> - No need as it centers the navbar*/}
+      <Navbar bg="light" expand="lg">
+        {/* <Container style={{ width: "100%" }}> - No need as it centers the navbar*/}
         <Navbar.Brand href="#home">NewsMonkey</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link> 
-            <Nav.Link href="#link">business</Nav.Link> 
-            <Nav.Link href="#link">entertainment</Nav.Link> 
-            <Nav.Link href="#link">general</Nav.Link> 
-            <Nav.Link href="#link">health</Nav.Link> 
-            <Nav.Link href="#link">science</Nav.Link> 
-            <Nav.Link href="#link">sports</Nav.Link> 
-            <Nav.Link href="#link">technology</Nav.Link> 
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/link">About </Nav.Link>
+            <Link className="nav-link" to="/business">
+              Business
+            </Link>
+            <Link className="nav-link" to="/entertainment">
+              Entertainment
+            </Link>
+            <Link className="nav-link" to="/">
+              General
+            </Link>
+            <Link className="nav-link" to="/health">
+              Health
+            </Link>
+            <Link className="nav-link" to="/science">
+              Science
+            </Link>
+            <Link className="nav-link" to="/sports">
+              Sports
+            </Link>
+            <Link className="nav-link" to="/technology">
+              Technology
+            </Link>
           </Nav>
         </Navbar.Collapse>
-      {/* </Container> */}
-    </Navbar>
-      
-    )
+        {/* </Container> */}
+      </Navbar>
+    );
   }
 }
