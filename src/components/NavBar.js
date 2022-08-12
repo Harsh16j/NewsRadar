30729a880cf98b217bb6983d4b0fb650f937dd1e
Harsh <harsh.jethwani16@gmail.com>
2022-08-12 12:49:55 -0500

    NavBar component converted to function based

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,42 +3,80 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 
-export default class NavBar extends Component {
-  render() {
-    return (
-      <Navbar bg="light" expand="lg">
-        {/* <Container style={{ width: "100%" }}> - No need as it centers the navbar*/}
-        <Navbar.Brand href="#home">NewsMonkey</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/link">About </Nav.Link>
-            <Link className="nav-link" to="/business">
-              Business
-            </Link>
-            <Link className="nav-link" to="/entertainment">
-              Entertainment
-            </Link>
-            <Link className="nav-link" to="/">
-              General
-            </Link>
-            <Link className="nav-link" to="/health">
-              Health
-            </Link>
-            <Link className="nav-link" to="/science">
-              Science
-            </Link>
-            <Link className="nav-link" to="/sports">
-              Sports
-            </Link>
-            <Link className="nav-link" to="/technology">
-              Technology
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-        {/* </Container> */}
-      </Navbar>
-    );
-  }
+export default function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      {/* <Container style={{ width: "100%" }}> - No need as it centers the navbar*/}
+      <Navbar.Brand href="#home">NewsMonkey</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/link">About </Nav.Link>
+          <Link className="nav-link" to="/business">
+            Business
+          </Link>
+          <Link className="nav-link" to="/entertainment">
+            Entertainment
+          </Link>
+          <Link className="nav-link" to="/">
+            General
+          </Link>
+          <Link className="nav-link" to="/health">
+            Health
+          </Link>
+          <Link className="nav-link" to="/science">
+            Science
+          </Link>
+          <Link className="nav-link" to="/sports">
+            Sports
+          </Link>
+          <Link className="nav-link" to="/technology">
+            Technology
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+      {/* </Container> */}
+    </Navbar>
+  );
 }
+
+// export default class NavBar extends Component {
+//   render() {
+//     return (
+//       <Navbar bg="light" expand="lg">
+//         {/* <Container style={{ width: "100%" }}> - No need as it centers the navbar*/}
+//         <Navbar.Brand href="#home">NewsMonkey</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link href="/home">Home</Nav.Link>
+//             <Nav.Link href="/link">About </Nav.Link>
+//             <Link className="nav-link" to="/business">
+//               Business
+//             </Link>
+//             <Link className="nav-link" to="/entertainment">
+//               Entertainment
+//             </Link>
+//             <Link className="nav-link" to="/">
+//               General
+//             </Link>
+//             <Link className="nav-link" to="/health">
+//               Health
+//             </Link>
+//             <Link className="nav-link" to="/science">
+//               Science
+//             </Link>
+//             <Link className="nav-link" to="/sports">
+//               Sports
+//             </Link>
+//             <Link className="nav-link" to="/technology">
+//               Technology
+//             </Link>
+//           </Nav>
+//         </Navbar.Collapse>
+//         {/* </Container> */}
+//       </Navbar>
+//     );
+//   }
+// }
