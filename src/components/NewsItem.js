8c14f4c6e548c1adb25a183f64964ee8287ce077
Harsh <harsh.jethwani16@gmail.com>
2022-08-12 13:16:02 -0500

    NewsItem converted into Functional component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,48 +1,90 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageURL, newsURL, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <Card>
-          <div style={{display: 'flex',
-    justifyContent: 'flex-end',
-    position: 'absolute',
-    right: "0"}}>
-          <span className="badge rounded-pill bg-danger">
-            {source}
-          </span>
-          </div>
-          <Card.Img
-            variant="top"
-            src={
-              imageURL
-                ? imageURL
-                : "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
-            }
-            style={{ width: "100%", height: "200px" }}
-          />
-          {/* To fix the size of the image w.r.t. the cards */}
-          <Card.Body>
-            <Card.Title>{title?title.slice(0, 45):" "}</Card.Title>
-            <Card.Text>
-              {description == null ? "---" : description.slice(0, 88)}
-            </Card.Text>
-            <p className="text-muted">
-              By {author ? author : "Unknown"} on {new Date(date).toGMTString()}
-            </p>
-            <Button variant="primary" size="sm" href={newsURL} target="_blank">
-              Read more
-            </Button>
-          </Card.Body>
-        </Card>
-      </div>
-    );
-  }
+export default function NewsItem(props) {
+  let { title, description, imageURL, newsURL, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <Card>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <Card.Img
+          variant="top"
+          src={
+            imageURL
+              ? imageURL
+              : "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
+          }
+          style={{ width: "100%", height: "200px" }}
+        />
+        {/* To fix the size of the image w.r.t. the cards */}
+        <Card.Body>
+          <Card.Title>{title ? title.slice(0, 45) : " "}</Card.Title>
+          <Card.Text>
+            {description == null ? "---" : description.slice(0, 88)}
+          </Card.Text>
+          <p className="text-muted">
+            By {author ? author : "Unknown"} on {new Date(date).toGMTString()}
+          </p>
+          <Button variant="primary" size="sm" href={newsURL} target="_blank">
+            Read more
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
-export default NewsItem;
+// export class NewsItem extends Component {
+//   render() {
+//     let { title, description, imageURL, newsURL, author, date, source } =
+//       this.props;
+//     return (
+//       <div className="my-3">
+//         <Card>
+//           <div style={{display: 'flex',
+//     justifyContent: 'flex-end',
+//     position: 'absolute',
+//     right: "0"}}>
+//           <span className="badge rounded-pill bg-danger">
+//             {source}
+//           </span>
+//           </div>
+//           <Card.Img
+//             variant="top"
+//             src={
+//               imageURL
+//                 ? imageURL
+//                 : "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
+//             }
+//             style={{ width: "100%", height: "200px" }}
+//           />
+//           {/* To fix the size of the image w.r.t. the cards */}
+//           <Card.Body>
+//             <Card.Title>{title?title.slice(0, 45):" "}</Card.Title>
+//             <Card.Text>
+//               {description == null ? "---" : description.slice(0, 88)}
+//             </Card.Text>
+//             <p className="text-muted">
+//               By {author ? author : "Unknown"} on {new Date(date).toGMTString()}
+//             </p>
+//             <Button variant="primary" size="sm" href={newsURL} target="_blank">
+//               Read more
+//             </Button>
+//           </Card.Body>
+//         </Card>
+//       </div>
+//     );
+//   }
+// }
+
+// export default NewsItem;
